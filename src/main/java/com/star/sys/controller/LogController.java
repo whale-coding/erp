177a2cc4920ad9a6898d95312b63d7626a77efb6
计科192-聂建强 <859193125@qq.com>
2021-08-10 10:30:24 +0800

    日志管理——查询功能

--- src/main/java/com/star/sys/controller/LogController.java
+++ src/main/java/com/star/sys/controller/LogController.java
@@ -8,9 +8,9 @@ import com.star.sys.pojo.Log;
 import com.star.sys.service.LogService;
 import com.star.sys.utils.DataGridViewResult;
 import com.star.sys.vo.LogVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -37,6 +37,19 @@ public class LogController {
 
         //创建条件构造器
         QueryWrapper<Log> wrapper = new QueryWrapper<>();
+        /**
+         * 参数1：条件  参数2：数据库列名  参数3：值
+         */
+        //操作类型
+        wrapper.eq(StringUtils.isNotBlank(logVo.getType()),"type",logVo.getType());
+        //登录名模糊查询
+        wrapper.like(StringUtils.isNotBlank(logVo.getLoginname()),"loginname",logVo.getLoginname());
+        //开始时间
+        wrapper.ge(logVo.getStartTime()!=null,"createtime",logVo.getStartTime());
+        //结束时间
+        wrapper.le(logVo.getEndTime()!=null,"createtime",logVo.getEndTime());
+        //设置排序
+        wrapper.orderByDesc("createtime");//登录时间降序
 
         //调用查询日志列表的方法
         IPage<Log> logIPage = logService.page(page, wrapper);
