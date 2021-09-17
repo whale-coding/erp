490244a4d2c758e19a17266b4cec6cf8ef2da6f5
code-rabbits <niejianqiang1226@gmail.com>
2021-09-17 16:49:11 +0800

    商品管理完成

--- src/main/java/com/star/bus/controller/GoodsController.java
+++ src/main/java/com/star/bus/controller/GoodsController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -157,8 +158,6 @@ public class GoodsController {
             return SystemConstant.DELETE_ERROR;
         }
     }
-
-
 
 
 
